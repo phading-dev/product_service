3cb66c89a7e18ae54a6cb7089e3a991be6fa8de6
Ykuyo <ykuyo.dev@gmail.com>
2025-07-26 13:08:46 -0700

    Added prod env.

---
+++ prod/env.ts
@@ -0,0 +1,12 @@
+import "../env_const";
+import "@phading/cluster/prod/env";
+import { ENV_VARS } from "../env_vars";
+
+ENV_VARS.spannerInstanceId = ENV_VARS.balancedSpannerInstanceId;
+ENV_VARS.r2SeasonCoverImageBucketName = "season-cover-image";
+ENV_VARS.r2SeasonCoverImagePublicAccessOrigin =
+  "https://season-cover-image.secount.com";
+ENV_VARS.r2VideoPublicAccessOrigin = "https://video.secount.com";
+ENV_VARS.replicas = 2;
+ENV_VARS.cpu = "300m";
+ENV_VARS.memory = "512Mi";
